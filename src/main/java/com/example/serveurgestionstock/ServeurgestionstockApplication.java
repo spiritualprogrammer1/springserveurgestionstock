e60c5b65833286eb312b1d9b0b807f641815a07c
geoffroy <contact.geoffroy@gmail.com>
2019-05-05 04:32:06 +0200

    emplacement  des files

---
+++ src/main/java/com/example/serveurgestionstock/ServeurgestionstockApplication.java
@@ -0,0 +1,13 @@
+package com.example.serveurgestionstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurgestionstockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeurgestionstockApplication.class, args);
+    }
+
+}
